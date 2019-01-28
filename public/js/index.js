995bd44323ce3170ee54b6b879abd3f090796a17
muhammadali448 <muhammadali19960@outlook.com>
2019-01-28 16:44:31 +0500

    implementing html form

--- public/js/index.js
+++ public/js/index.js
@@ -8,18 +8,33 @@ socket.on("connect", function() {
 });
 socket.on("newMessage", function(message) {
   console.log("newMessage", message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
 });
 
-socket.emit("createMessage", {
-  from: "Raja Jee",
-  text: "Hello Frndx Chai peelo"
-}, function(data) {
-  console.log('Got it ', data);
-});
+// socket.emit("createMessage", {
+//   from: "Raja Jee",
+//   text: "Hello Frndx Chai peelo"
+// }, function(data) {
+//   console.log('Got it ', data);
+// });
 
 socket.on("disconnect", function() {
   console.log("disconnected from server");
 });
+
+jQuery('#message-form').on('submit', function (e) { 
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  })
+ })
+
+
 
 // socket.on('newEmail', function(email) {
 //   console.log('New Email', email);
